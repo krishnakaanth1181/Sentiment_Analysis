ce41d88846118c8de82bec06de96ee8ad69214be
krishnakaanth1181 <krishnakaanth1181@gmail.com>
2026-04-09 10:07:43 +0530

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ if st.button("Analyze Sentiment"):
             # call your Flask backend
             response = requests.post(
                 "http://127.0.0.1:5000/predict",
-                json = {"reviews",review}
+                json = {"reviews":review}
             )
             result = response.json()
 
